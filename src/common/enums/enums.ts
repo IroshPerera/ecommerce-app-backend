3648b2fe54c6bf2c333239b15d9a3d4b109d5a05
iroshperera <iroshlashan31@gmail.com>
2025-03-20 15:12:40 +0530

    implement enums

--- src/common/enums/enums.ts
+++ src/common/enums/enums.ts
@@ -21,6 +21,7 @@ export enum Category{
     GOOGLE = 'GOOGLE',
 }
 
+
 export enum ProductSortBy{
     PRICE_ASC = 'PRICE_ASC',
     PRICE_DESC = 'PRICE_DESC',
@@ -29,3 +30,29 @@ export enum ProductSortBy{
     CREATED_AT_ASC = 'CREATED_AT_ASC',
     CREATED_AT_DESC = 'CREATED_AT_DESC',
 }
+
+export enum OrderStatus{
+    PENDING = 'PENDING',
+    COMPLETED = 'COMPLETED',
+    CANCELLED = 'CANCELLED',
+}
+
+export enum PaymentMethod{
+    CASH = 'CASH',
+    CASH_ON_DELIVERY = 'CASH_ON_DELIVERY',
+    CARD = 'CARD',
+    
+}
+
+export enum PaymentStatus {
+    PENDING = 'PENDING',
+    COMPLETED = 'COMPLETED',
+    FAILED = 'FAILED',
+    REFUNDED = 'REFUNDED',
+  };
+
+  export enum DeliveryStatus {
+    PENDING = 'PENDING',
+    DELIVERED = 'DELIVERED',
+    CANCELED = 'CANCELED',
+  };

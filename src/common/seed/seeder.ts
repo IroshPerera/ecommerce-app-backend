482c3ced76fad3668163cb94187b636d0e0ef31c
iroshperera <iroshlashan31@gmail.com>
2025-03-20 15:13:55 +0530

    complete seeders

--- src/common/seed/seeder.ts
+++ src/common/seed/seeder.ts
@@ -1,5 +1,6 @@
 import connectDB from "../../config/db";
 import { seedCategories } from "./data/category.seed";
+import { seedColors } from "./data/color.seed";
 import { seedRoles } from "./data/role.seed";
 import { seedAdmin } from "./data/user.seed";
 
@@ -7,9 +8,10 @@ import { seedAdmin } from "./data/user.seed";
 async function seedDatabase() {
     try {
         await connectDB();
-        await seedAdmin();
         await seedCategories();
         await seedRoles();
+        await seedColors();
+        await seedAdmin();
         console.log("Database Seeded Successfully.");
     } catch (error) {
         console.error("Database Seeder Error: ", error);
